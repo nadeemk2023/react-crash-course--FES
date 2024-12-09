ad17bafc0c23c5eed6c9b66a42b218f39d3bc4df
nadeemk2023 <nadeemekhan@gmail.com>
2024-12-08 19:31:16 -0800

    Add Todo component and update App to use it for displaying tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import './App.css';
+import Todo from './components/Todo.jsx';
 
 function App() {
   return (
     <>
       <h1>My Todo List</h1>
-      <div>
-        <h2>Finish Frontend Simplified</h2>
-        <span>Description</span>
-        <button>Delete</button>
-      </div>
-      <div>
-        <h2>Finish Interview Section</h2>
-        <button>Delete</button>
-      </div>
-      <div>
-        <h2>Land $100k Job</h2>
-        <button>Delete</button>
-      </div>
+      <Todo />
+      <Todo />
+      <Todo />
     </>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,11 @@
+function Todo() {
+  return (
+    <div>
+      <h2>Finish Frontend Simplified</h2>
+      <span>Description</span>
+      <button>Delete</button>
+    </div>
+  );
+}
+
+export default Todo;
