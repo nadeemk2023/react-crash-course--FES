d5dd63ce1d7f6e651c12abed89268eb5819365b5
nadeemk2023 <nadeemekhan@gmail.com>
2024-12-10 04:59:15 -0800

    Refactor App component to manage modal visibility with state and update button action to show modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import './App.css';
 import Todo from './components/Todo.jsx';
 import Title from './components/Title.jsx';
 import Modal from './components/Modal.jsx';
+import { useState } from 'react';
 
 function App() {
-  let isModalOpen = false;
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
@@ -14,14 +15,14 @@ function App() {
           type='text'
           onChange={event => console.log(event.target.value)}
         />
-        <button>Add todo</button>
+        <button onClick={() => setShowModal(true)}>Add todo</button>
       </div>
       <div className='todo__wrapper'>
         <Todo title='Finish Frontend Simplified' />
         <Todo title='Finish Interview Section' />
         <Todo title='Land a $100k Job' />
       </div>
-      {isModalOpen ? <Modal title='Confirm delete?' /> : null}
+      {showModal && <Modal title='Confirm delete?' />}
     </>
   );
 }
