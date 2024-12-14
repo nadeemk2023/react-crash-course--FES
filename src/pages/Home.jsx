0f2eaf6ce5fc69fdd464d0ff1cefb55cd600fb6a
nadeemk2023 <nadeemekhan@gmail.com>
2024-12-14 15:34:52 -0800

    Add axios dependency and implement user fetching in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 function Home() {
+  useEffect(() => {
+    async function fetchUsers() {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      console.log(response);
+    }
+    fetchUsers();
+  }, []);
   return <h1>Home</h1>;
 }
 
