fdf946b47ea03084aaec55a939ff2f7b1483abee
nadeemk2023 <nadeemekhan@gmail.com>
2024-12-14 15:36:28 -0800

    Refactor user fetching in Home component to define fetchUsers function outside useEffect for readability

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,14 @@ import { useEffect } from 'react';
 import axios from 'axios';
 
 function Home() {
+  async function fetchUsers() {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+    console.log(response);
+  }
+
   useEffect(() => {
-    async function fetchUsers() {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/users'
-      );
-      console.log(response);
-    }
     fetchUsers();
   }, []);
   return <h1>Home</h1>;
