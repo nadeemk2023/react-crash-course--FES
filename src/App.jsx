941c7bd4597f32cb9ab4035e801859f6b9f3d0b5
nadeemk2023 <nadeemekhan@gmail.com>
2024-12-18 23:05:31 -0800

    Refactor Users component to simplify user fetching and update display format

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Contact from './pages/Contact.jsx';
-import Nav from './components/Nav.jsx';
 import Users from './pages/Users.jsx';
 
+// import Nav from './components/Nav.jsx';
 // import { useState, useEffect } from 'react';
 // import Todo from './components/Todo.jsx';
 // import Title from './components/Title.jsx';
@@ -15,7 +15,7 @@ import Users from './pages/Users.jsx';
 function App() {
   return (
     <Router>
-      <Nav />
+      {/* <Nav /> */}
       <Routes>
         <Route path='/' element={<Home />}></Route>
         {/* <Route path='/about' element={<About />} />
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,27 +1,52 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 function Users() {
   const { id } = useParams();
   const [user, setUser] = useState({});
-  const { name, email, username } = user;
 
-  const fetchUser = async () => {
+  //My Solution
+  //   const [user, setUser] = useState({});
+  //   const { name, email, username } = user;
+  //
+  //   const fetchUser = async () => {
+  //     const { data } = await axios.get(
+  //       `https://jsonplaceholder.typicode.com/users/${id}`
+  //     );
+  //     setUser(data);
+  //   };
+  //
+  //   useEffect(() => {
+  //     fetchUser();
+  //   }, [id]);
+  //   return (
+  //     <div style={{ textAlign: 'center', width: '100vw' }}>
+  //       <h1>{name}</h1>
+  //       <p>{email}</p>
+  //       <p>{username}</p>
+  //     </div>
+  //   );
+  //-------------------------------------------------------
+
+  // David's Solution
+  async function fetchUser() {
     const { data } = await axios.get(
       `https://jsonplaceholder.typicode.com/users/${id}`
     );
     setUser(data);
-  };
-
+  }
   useEffect(() => {
     fetchUser();
-  }, [id]);
+  }, []);
+
   return (
-    <div style={{ textAlign: 'center', width: '100vw' }}>
-      <h1>{name}</h1>
-      <p>{email}</p>
-      <p>{username}</p>
+    <div>
+      <Link to='/'>Go Back</Link>
+      <p>{user.name}</p>
+      <p>{user.email}</p>
+      <p>{user.id}</p>
+      <p>{user.username}</p>
     </div>
   );
 }
